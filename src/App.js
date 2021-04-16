83e98ae14bab20155660a8c17ce87c78a4799a71
Jaron-Lane <jaronwohlwend@icloud.com>
2021-04-16 17:32:42 -0500

    Created button for uploading

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+
+  const uploadImage = async e => {
+    
+  }
+
   return (
     <div className="App">
-      
+      <h1>Upload Images to Cloudinary with React</h1>
+      <input type="file" placeholder="Upload an Image"
+      onChange={uploadImage} />
     </div>
   );
 }
