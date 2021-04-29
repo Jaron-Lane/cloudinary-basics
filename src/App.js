e1fa2b6d70933c237879f7fecc613885788bcce2
Jaron-Lane <jaronwohlwend@icloud.com>
2021-04-29 18:11:32 -0500

    Completed cloudinary tutorial

--- src/App.js
+++ src/App.js
@@ -8,10 +8,23 @@ function App() {
 
   const uploadImage = async e => {
     const files = e.target.files
-    const data = FormData()
+    const data = new FormData()
     data.append("file", files[0])
     data.append("upload_preset", "practiceimages")
     setLoading(true)
+
+    const res = await fetch("https://api.cloudinary.com/v1_1/dvtukfool/image/upload", 
+    {
+      method: "POST",
+      body: data
+    })
+
+    const file = await res.json()
+
+    console.log(file)
+
+    setImage(file.secure_url)
+    setLoading(false)
   }
 
   return (
@@ -19,6 +32,10 @@ function App() {
       <h1>Upload Images to Cloudinary with React</h1>
       <input type="file" name="file" placeholder="Upload an Image"
       onChange={uploadImage} />
+
+      {
+        loading ? (<h3>Loading ...</h3>) : (<img src={image} style={{width:'300px'}} />)
+      }
     </div>
   );
 }
