fa7f61674561020a151262bb5c434e11c6836e31
Jaron-Lane <jaronwohlwend@icloud.com>
2021-04-29 17:49:27 -0500

    created useState variables

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
 
+  const [loading, setLoading] = useState(false)
+  const [image, setImage] = useState("")
+
   const uploadImage = async e => {
-    
+    const files = e.target.files
+    const data = FormData()
+    data.append("file", files[0])
+    data.append("upload_preset", "practiceimages")
+    setLoading(true)
   }
 
   return (
     <div className="App">
       <h1>Upload Images to Cloudinary with React</h1>
-      <input type="file" placeholder="Upload an Image"
+      <input type="file" name="file" placeholder="Upload an Image"
       onChange={uploadImage} />
     </div>
   );
